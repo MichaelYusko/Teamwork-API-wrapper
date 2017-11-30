f838aa143730b89617ab67db866cc302eda575b9
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-11-30 23:17:01 +0200

    Add a minimal structure of project

---
+++ tests/unit/test_exceptions.py
@@ -0,0 +1,11 @@
+import pytest
+
+from src.teamwork.client import Teamwork
+from src.teamwork.exceptions import CredentialsError, CREDENTIALS_MESSAGE
+
+
+def test_credential_raises():
+    """Test for credentials, if not provided raise the Error"""
+    with pytest.raises(CredentialsError) as error:
+        Teamwork()
+    assert str(error.value) == CREDENTIALS_MESSAGE
