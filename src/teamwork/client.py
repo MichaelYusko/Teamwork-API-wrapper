acf789259400a5658ea72092be498562e2db898c
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-12-01 17:13:45 +0200

    Add Auth class

--- src/teamwork/client.py
+++ src/teamwork/client.py
@@ -1,5 +1,8 @@
 """Client stuff"""
 
+from requests import Session
+
+from .constants import AUTHENTICATE_URL
 from .exceptions import CredentialsError  # pylint: disable=import-error
 
 
@@ -10,9 +13,39 @@ class BaseTeamwork:  # pylint: disable=too-few-public-methods
             raise CredentialsError
 
         self._credentials = (username, password)
+        self.session = Session()
+        self._auth()
+
+    def _auth(self):
+        """Auth via Basic authentication"""
+        self.session.auth = self._credentials
+        self.session.get(AUTHENTICATE_URL)
+
+    def _get(self, url, **kwargs):
+        """ Custom get method
+        :param url: An endpoint, which we need to call.
+        :param kwargs: Another keys
+        :return: An dictionary object
+        """
+        return self.session.get(url, **kwargs).json()
+
+
+class Auth(BaseTeamwork):  # pylint: disable=too-few-public-methods
+    """Authentication stuff"""
+
+    def __init__(self, username, password):  # pylint: disable=useless-super-delegation
+        super().__init__(username, password)
+
+    @property
+    def my_account(self):
+        """ Get own account details
+        :return: An dictionary with information
+        """
+        return self._get(AUTHENTICATE_URL)
 
 
 class Teamwork(BaseTeamwork):  # pylint: disable=too-few-public-methods
     """Main class"""
     def __init__(self, username=None, password=None):  # pylint: disable=useless-super-delegation
         super().__init__(username, password)
+        self.auth = Auth(username, password)
---
+++ src/teamwork/constants.py
@@ -0,0 +1,3 @@
+"""Constants stuff"""
+
+AUTHENTICATE_URL = 'https://authenticate.teamwork.com/authenticate.json'
