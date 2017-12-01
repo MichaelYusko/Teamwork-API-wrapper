1a59b713b4df9f251e68f52349d2e100d83da01e
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-12-01 21:51:59 +0200

    Add team argument

--- src/teamwork/client.py
+++ src/teamwork/client.py
@@ -2,24 +2,25 @@
 
 from requests import Session
 
-from .constants import AUTHENTICATE_URL
+from .constants import BASE_URL
 from .exceptions import CredentialsError  # pylint: disable=import-error
 
 
 class BaseTeamwork:  # pylint: disable=too-few-public-methods
     """Base Teamwork class that includes the most used methods"""
-    def __init__(self, username, password):
+    def __init__(self, username, password, team):
         if not username or not password:
             raise CredentialsError
 
         self._credentials = (username, password)
         self.session = Session()
         self._auth()
+        self.team = team
 
     def _auth(self):
         """Auth via Basic authentication"""
         self.session.auth = self._credentials
-        self.session.get(AUTHENTICATE_URL)
+        self.session.get(BASE_URL)
 
     def _get(self, url, **kwargs):
         """ Custom get method
@@ -33,19 +34,19 @@ class BaseTeamwork:  # pylint: disable=too-few-public-methods
 class Auth(BaseTeamwork):  # pylint: disable=too-few-public-methods
     """Authentication stuff"""
 
-    def __init__(self, username, password):  # pylint: disable=useless-super-delegation
-        super().__init__(username, password)
+    def __init__(self, username, password, team):  # pylint: disable=useless-super-delegation
+        super().__init__(username, password, team)
 
     @property
     def my_account(self):
         """ Get own account details
         :return: An dictionary with information
         """
-        return self._get(AUTHENTICATE_URL)
+        return self._get(BASE_URL)
 
 
 class Teamwork(BaseTeamwork):  # pylint: disable=too-few-public-methods
     """Main class"""
-    def __init__(self, username=None, password=None):  # pylint: disable=useless-super-delegation
-        super().__init__(username, password)
-        self.auth = Auth(username, password)
+    def __init__(self, username=None, password=None, team=None):  # pylint: disable=useless-super-delegation
+        super().__init__(username, password, team)
+        self.auth = Auth(username, password, team)
--- tests/unit/test_init_object.py
+++ tests/unit/test_init_object.py
@@ -2,4 +2,5 @@
 def test_init_obj(client):
     """Test if client was setup correctly"""
     key, password = client._credentials[0], client._credentials[1]
-    assert ('MY_KEY', 'MY_PASS') == (key, password)
+    team = client.team
+    assert ('MY_KEY', 'MY_PASS', 'MY_TEAM') == (key, password, team)
