e1683dc490f084a23bb8f720a255158de1766987
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-12-02 20:07:36 +0200

    Add activity class

--- src/teamwork/client.py
+++ src/teamwork/client.py
@@ -4,6 +4,7 @@ from requests import Session
 
 from .constants import BASE_URL
 from .exceptions import CredentialsError  # pylint: disable=import-error
+from .utils import make_url
 
 
 class BaseTeamwork:  # pylint: disable=too-few-public-methods
@@ -45,8 +46,29 @@ class Auth(BaseTeamwork):  # pylint: disable=too-few-public-methods
         return self._get(BASE_URL)
 
 
+class Activity(BaseTeamwork):
+    def __init__(self, username, password, team):
+        super().__init__(username, password, team)
+
+    _action = 'latestActivity'
+
+    def last(self, max_items=60, only_stared=False):
+        """Lists the latest activity across all projects ordered chronologically
+        :param max_items: Count of items
+        :param only_stared: Return stared projects
+        :return: an array with objects
+        """
+        url = make_url(self.team, self._action)
+        params = {'maxItems': max_items, 'onlyStarred': only_stared}
+
+        response = self._get(url, params=params)
+        all_projects = response['activity']
+        return all_projects
+
+
 class Teamwork(BaseTeamwork):  # pylint: disable=too-few-public-methods
     """Main class"""
     def __init__(self, username=None, password=None, team=None):
         super().__init__(username, password, team)
         self.auth = Auth(username, password, team)
+        self.activity = Activity(username, password, team)
